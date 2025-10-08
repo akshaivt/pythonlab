80f0f491a1d48f30ba9277f608facefb6770a725
akshaivt <akshaivt@gmail.com>
2025-10-08 12:20:19 +0530

    done

--- exchange.py
+++ exchange.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-6
-=======
 s = input("Enter a string: ")
-new_s = s [-1] + s(1:-1] + s[0] if len(s) > 1 else s 
-print("Result:" new_s)
->>>>>>> refs/remotes/origin/main
+new_s = s[-1] + s[1:-1] + s[0] if len(s) > 1 else s
+print("Result:", new_s)
+
