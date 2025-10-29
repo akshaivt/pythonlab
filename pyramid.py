a577da890fba14dbc1b881df53a770d48a8c376c
Akshai <akshaivt@gmail.com>
2025-10-29 12:38:16 +0530

    Add pyramid pattern printing functionality

---
+++ pyramid.py
@@ -0,0 +1,9 @@
+N=int(input("Enter numbers of steps: "))
+for i in range (1,N+1):
+    for j in range (1,i+1):
+        print(i*j,end=" ")
+    print()
+~                                                                                                                                                      
+~                                                                                                                                                      
+~                                                                                                                                                      
+~                      
